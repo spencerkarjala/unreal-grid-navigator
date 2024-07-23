18bcfb97b94ebe9b909d1edfd095a62c5b64ab34
Spencer Karjala <spencerkarjala@gmail.com>
2024-07-22 17:59:41 -0700

    add basic grid rendering component implementations
    Signed-off-by: Spencer Karjala <spencerkarjala@gmail.com>

--- Source/GridNavigator/Private/GNNavMeshRenderingComponent.h
+++ Source/GridNavigator/Private/GNNavMeshRenderingComponent.h
@@ -1,15 +1,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Components/PrimitiveComponent.h"
+#include "Debug/DebugDrawComponent.h"
 #include "GNNavMeshRenderingComponent.generated.h"
 
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
-class GRIDNAVIGATOR_API UGNNavMeshRenderingComponent : public UPrimitiveComponent
+class UGNNavMeshRenderingComponent : public UDebugDrawComponent
 {
 	GENERATED_BODY()
 
 public:
-	UGNNavMeshRenderingComponent() = default;
+	// ReSharper disable once CppNonExplicitConvertingConstructor
+	UGNNavMeshRenderingComponent(const FObjectInitializer& ObjectInitializer);
+
+	virtual void OnRegister() override;
+	virtual void OnUnregister() override;
+
+	virtual FDebugRenderSceneProxy* CreateDebugSceneProxy() override;
+	virtual FDebugDrawDelegateHelper& GetDebugDrawDelegateHelper() override;
+
+	virtual FBoxSphereBounds CalcBounds(const FTransform& LocalToWorld) const override;
+
+private:
+	bool CheckShowNavigationFlag() const;
 };
