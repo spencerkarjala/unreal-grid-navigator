fd890669fa0aae0322dcca50c92a8ddcb15b55f1
Spencer Karjala <spencerkarjala@gmail.com>
2024-07-28 13:56:29 -0700

    resolve bug where walls directly on top of node would be detected as short ceilings
    Signed-off-by: Spencer Karjala <spencerkarjala@gmail.com>

--- Source/GridNavigator/Private/MappingServer.cpp
+++ Source/GridNavigator/Private/MappingServer.cpp
@@ -255,7 +255,9 @@ void FMappingServer::PopulateMap(const UWorld& World, const FBox& BoundingBox)
 			const int Layer = FMath::RoundToInt(HitResult.Location.Z / 25.0);
 			const bool bShortRoofOverHitPoint = FloorTrace(i, j, Layer, Layer + GridNavigatorConfig::MinEmptyLayersForValidNode, CeilHitResult, World);
 
-			if (bShortRoofOverHitPoint) {
+			// end loop if there's nowhere to stand; also, ignore walls that might be directly
+			// on top of the node (ie. have a near-zero Z component for normal vector)
+			if (bShortRoofOverHitPoint && CeilHitResult.Normal.Z > 1.0) {
 				continue;
 			}
 			
