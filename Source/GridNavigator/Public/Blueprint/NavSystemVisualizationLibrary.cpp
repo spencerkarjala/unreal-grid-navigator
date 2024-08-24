0aab528c024fd1283dd56a3137f7b79e31e07e47
Spencer Karjala <spencerkarjala@gmail.com>
2024-08-24 16:08:59 -0700

    hook bp library into world initialization
    Signed-off-by: Spencer Karjala <spencerkarjala@gmail.com>

--- Source/GridNavigator/Public/Blueprint/NavSystemVisualizationLibrary.cpp
+++ Source/GridNavigator/Public/Blueprint/NavSystemVisualizationLibrary.cpp
@@ -4,18 +4,50 @@
 
 DECLARE_LOG_CATEGORY_CLASS(LogNavSystemVisualizationLibrary, Log, All);
 
-const TArray<TObjectPtr<ANavigationData>>& UNavSystemVisualizationLibrary::GetNavigationData(TObjectPtr<UWorld> WorldInstance)
+TObjectPtr<UWorld> UNavSystemVisualizationLibrary::SavedWorldInstance = nullptr;
+
+void UNavSystemVisualizationLibrary::PostInitProperties()
 {
-	if (!WorldInstance) {
-		UE_LOG(LogNavSystemVisualizationLibrary, Error, TEXT("Received invalid world reference when trying to GetNavigationData"));
-		return {};
+	Super::PostInitProperties();
+
+	PostWorldInitDelegateHandle = FWorldDelegates::OnPostWorldInitialization.AddStatic(&HandlePostWorldInitialization);
+}
+
+void UNavSystemVisualizationLibrary::FinishDestroy()
+{
+	Super::FinishDestroy();
+
+	FWorldDelegates::OnPostWorldInitialization.Remove(PostWorldInitDelegateHandle);
+}
+
+bool UNavSystemVisualizationLibrary::GetNavigationData(const UWorld* WorldInstance, TArray<ANavigationData*>& NavData)
+{
+	const auto* NavSysBase = WorldInstance ? WorldInstance->GetNavigationSystem() : nullptr;
+	const auto* NavSys = Cast<UNavigationSystemV1>(NavSysBase);
+	if (!NavSys) {
+		UE_LOG(LogNavSystemVisualizationLibrary, Error, TEXT("Failed to retrieve navigation system when trying to GetNavigationData"));
+		return false;
 	}
 
-	const auto NavSys = Cast<UNavigationSystemV1>(WorldInstance->GetNavigationSystem());
+	for (auto DataItem : NavSys->NavDataSet) {
+		NavData.Push(DataItem);
+	}
+	return true;
+}
+
+void UNavSystemVisualizationLibrary::HandlePostWorldInitialization(UWorld* World, FWorldInitializationValues WorldInitValues)
+{
+	SavedWorldInstance = World;
+	if (!SavedWorldInstance) {
+		UE_LOG(LogNavSystemVisualizationLibrary, Error, TEXT("Received invalid world refrence when trying to HandlePostWorldInitialization"));
+		return;
+	}
+	
+	const auto* NavSys = SavedWorldInstance->GetNavigationSystem();
 	if (NavSys == nullptr) {
-		UE_LOG(LogNavSystemVisualizationLibrary, Error, TEXT("Couldn't retrieve navigation system when trying to GetNavigationData"));
-		return {};
+		UE_LOG(LogNavSystemVisualizationLibrary, Error, TEXT("Failed to retrieve navigation system when trying to HandlePostWorldInitialization"));
+		return;
 	}
 
-	return NavSys->NavDataSet;
+	
 }
--- Source/GridNavigator/Public/Blueprint/NavSystemVisualizationLibrary.h
+++ Source/GridNavigator/Public/Blueprint/NavSystemVisualizationLibrary.h
@@ -4,11 +4,30 @@
 #include "NavSystemVisualizationLibrary.generated.h"
 
 UCLASS(Blueprintable, MinimalAPI)
-class GRIDNAVIGATOR_API UNavSystemVisualizationLibrary : public UBlueprintFunctionLibrary
+class UNavSystemVisualizationLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
 public:
-	UFUNCTION(BlueprintPure, Category = "Navigation", DisplayName = "Get Navigation Data")
-	static const TArray<TObjectPtr<ANavigationData>>& GetNavigationData(TObjectPtr<UWorld> WorldInstance);
+
+	virtual void PostInitProperties() override;
+	virtual void FinishDestroy() override;
+	
+	/**
+	 * @brief Exposes access to NavigationData actors that are active in the navigation system.
+	 * 
+	 * @param WorldInstance A reference to the current World instance
+	 * @param NavData The array to write navigation data to (expected to be empty)
+	 * @return true if the data was retrieved successfully; false otherwise
+	 *
+	 * @note This should be used for reading and hooking into data updates only
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Navigation", DisplayName = "Get Navigation Data")
+	static GRIDNAVIGATOR_API bool GetNavigationData(const UWorld* WorldInstance, TArray<ANavigationData*>& NavData);
+	
+	static TObjectPtr<UWorld> SavedWorldInstance;
+private:
+	static void HandlePostWorldInitialization(UWorld* World, FWorldInitializationValues WorldInitValues);
+
+
+	FDelegateHandle PostWorldInitDelegateHandle;
 };
