a9a822fc4cc1d061f340e5075f376d937ec2a9dc
Spencer Karjala <spencerkarjala@gmail.com>
2024-08-29 10:02:23 -0700

    remove unused mapping server subsystem
    Signed-off-by: Spencer Karjala <spencerkarjala@gmail.com>

--- Source/GridNavigator/Private/GNMappingServerSubsystem.cpp
+++
@@ -1,9 +0,0 @@
-#include "GNMappingServerSubsystem.h"
-
-#include "MappingServer.h"
-
-bool UGNMappingServerSubsystem::RequestPath(const FVector& FromPoint, const FVector& ToPoint, TArray<FVector>& OutPoints)
-{
-	OutPoints = FMappingServer::GetInstance().FindPath(FromPoint, ToPoint);
-	return true;
-}
